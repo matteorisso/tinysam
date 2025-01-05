d0f4d867eaf7a4c5b6d0aa8b2bbcb3c940abdcb7
matteorisso <matteorisso96@gmail.com>
2025-01-05 14:31:47 +0100

    Add `get_dense_pe` method to `PromptEncoder` to get the same positional encoding used to encode point prompts

--- efficientsam/efficient_sam_prompt_encoder.py
+++ efficientsam/efficient_sam_prompt_encoder.py
@@ -4,6 +4,7 @@ from typing import Tuple
 
 import numpy as np
 from tinygrad import nn
+from tinygrad.dtype import dtypes
 from tinygrad.tensor import Tensor
 
 
@@ -34,6 +35,17 @@ class PromptEncoder:
     self.point_embeddings = nn.Embedding(1, embed_dim)
     self.bbox_top_left_embeddings = nn.Embedding(1, embed_dim)
     self.bbox_bottom_right_embeddings = nn.Embedding(1, embed_dim)
+
+  def get_dense_pe(self) -> Tensor:
+    """
+    Returns the positional encoding used to encode point prompts,
+    applied to a dense set of points the shape of the image encoding.
+
+    Returns:
+      torch.Tensor: Positional encoding with shape
+        1x(embed_dim)x(embedding_h)x(embedding_w)
+    """
+    return self.pe_layer(self.image_embedding_size).unsqueeze(0)
 
   def _embed_points(
     self,
@@ -106,6 +118,19 @@ class PositionEmbeddingRandom:
   ) -> None:
     self.positional_encoding_gaussian_matrix = Tensor.uniform(2, num_pos_feats)
 
+  def __call__(self, size: Tuple[int, int]) -> Tensor:
+    """Generate positional encoding for a grid of the specified size."""
+    h, w = size
+    device = self.positional_encoding_gaussian_matrix.device
+    grid = Tensor.ones([h, w], device=device, dtype=dtypes.float32)
+    y_embed = grid.cumsum(axis=0).sub(-0.5)
+    x_embed = grid.cumsum(axis=1).sub(-0.5)
+    y_embed = y_embed.div(h)
+    x_embed = x_embed.div(w)
+
+    pe = self._pe_encoding(x_embed.stack(y_embed, dim=-1))
+    return pe.permute(2, 0, 1)  # C x H x W
+
   def _pe_encoding(self, coords: Tensor) -> Tensor:
     """Positionally encode points that are normalized to [0,1]."""
     # assuming coords are in [0, 1]^2 square and have d_1 x ... x d_n x 2 shape
@@ -124,6 +149,6 @@ class PositionEmbeddingRandom:
   ) -> Tensor:
     """Positionally encode points that are not normalized to [0,1]."""
     coords = coords_input.clone()
-    coords[:, :, 0] = coords[:, :, 0] / image_size[1]
-    coords[:, :, 1] = coords[:, :, 1] / image_size[0]
+    coords[:, :, 0] = coords[:, :, 0].div(image_size[1])
+    coords[:, :, 1] = coords[:, :, 1].div(image_size[0])
     return self._pe_encoding(coords)  # B x N x C
